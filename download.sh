d5a23c5d1487716d9e52a1a3c16b80a27e6ddece
Daniel Chettiar <74943095+1MochaChan1@users.noreply.github.com>
2026-05-21 23:07:23 +0530

    Fix download command and permissions for scripts

--- download.sh
+++ download.sh
@@ -1,1 +1,1 @@
-wget -O https://raw.githubusercontent.com/1MochaChan1/comfyui-workflows-and-scripts/main/comfyui-klein-firered.sh && chmode +x comfyui-klein-firered.sh
+-sSfL "http://raw.githubusercontent.com/1MochaChan1/comfyui-workflows-and-scripts/main/download.sh" -o download.sh && chmod +x download.sh
