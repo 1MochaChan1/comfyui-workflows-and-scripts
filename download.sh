d6ce12a9c29708a519c42ecd1904617246f94deb
Daniel Chettiar <74943095+1MochaChan1@users.noreply.github.com>
2026-05-22 15:06:25 +0530

    Replace curl with wget for downloading script

--- download.sh
+++ download.sh
@@ -1,1 +1,1 @@
--sSfL "http://raw.githubusercontent.com/1MochaChan1/comfyui-workflows-and-scripts/main/comfyui-klein-firered.sh" -o download.sh && chmod +x download.sh
+wget -q --show-progress -O download.sh "http://raw.githubusercontent.com/1MochaChan1/comfyui-workflows-and-scripts/main/comfyui-klein-firered.sh" && chmod +x download.sh
